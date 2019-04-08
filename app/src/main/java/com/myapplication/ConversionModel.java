acf62ae13cc0c54d90594a6779d50257af2aadaa
student <student@ssu-cs-dar25-167-138.sonoma.edu>
2019-04-07 18:57:21 -0700

    Added Model, Response file, Parser and json. Still need to fix get_json function in Parser. Goal is to open the file for use

---
+++ app/src/main/java/com/myapplication/ConversionModel.java
@@ -0,0 +1,22 @@
+package com.myapplication;
+
+public class ConversionModel {
+    private String input;
+    private String output;
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
---
+++ app/src/main/java/com/myapplication/ConversionResponse.java
@@ -0,0 +1,10 @@
+package com.myapplication;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ConversionResponse {
+//    private String character;
+//
+//    @SerializedName(character)
+//    private char inputChar;
+}
